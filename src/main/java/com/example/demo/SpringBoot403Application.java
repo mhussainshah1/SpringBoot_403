65dc3d5d647255ad568724fa3e21d961161a03e1
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-18 15:05:47 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot403Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot403Application.class, args);
+    }
+
+}
